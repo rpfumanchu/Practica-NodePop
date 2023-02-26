75fd1d75ba04b059b63c5044a50cd4c8b5e3965b
Rober <rp_rober@hotmail.com>
2023-02-26 23:01:49 +0100

    version final

--- nodepop/routes/api/validations.js
+++ nodepop/routes/api/validations.js
@@ -2,7 +2,7 @@ const {query, validationResult} = require('express-validator');
 
 const findOut = () => {
   return [
-    query("name").optional().withMessage("Este campo debe ser una cadena de texto"),
+    query("name").isString().optional().withMessage("Este campo debe ser una cadena de texto"),
     query("tags").isIn(["lifestyle","mobile","motor","work"]).optional().withMessage("Este campo no existe, prueba con: lifestyle,mobile,motor,work"),
     query("state").isBoolean().optional().withMessage("Este campo tiene que ser true o false"),
     query("skip",).isNumeric().optional().withMessage("Este campo tiene que ser numérico"),
